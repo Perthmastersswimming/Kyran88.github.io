dd3910e41441500a4492a0b275dfc4e5b138935d
Kyran88 <kmcmanus09@hotmail.com>
2016-08-10 14:40:17 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,15 +1,34 @@
-(function($) {
-  // variables
-  elementWidth = $('ul').width(),
-    containerWidth = $('nav').width(),
-    difference = elementWidth - containerWidth,
-    finalWidth = difference * 1.5,
-    element = $('ul');
 
-  // active on click
-  $('li').on('click', function() {
-    $('li').removeClass('active');
-    $(this).addClass('active');
-  });
+// look, click me!!
 
-})(jQuery);
+$("#header").on({
+  "click.header": function() {
+    $(".header__dev").toggleClass("header__dev--open");
+  }
+});
+
+
+setTimeout( function() {
+  $(".header__dev").addClass("header__dev--slow").removeClass("header__dev--open");
+},1000);
+
+
+setTimeout( function() {
+  $(".header__dev").removeClass("header__dev--slow");
+},2400);
+
+
+$(document).ready(function () {
+    $(".link-btn a").click(function () {
+        $(".overlay").fadeToggle(200);
+        $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+    $('.overlay').on('click', function () {
+        $(".overlay").fadeToggle(200);
+        $(".link-btn a").toggleClass('btn-open').toggleClass('btn-close');
+    });
+    $('.link a').on('click', function () {
+        $(".overlay").fadeToggle(200);
+        $(".link-btn a").toggleClass('btn-open').toggleClass('btn-close');
+    });
+});
