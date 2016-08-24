93053b2f3ed2e7fe56e69ecef88012aeacca5352
Kyran88 <kmcmanus09@hotmail.com>
2016-08-24 15:19:26 +0100

    Create index.js

---
+++ Nutrition/index.js
@@ -0,0 +1,23 @@
+eval(function(p, a, c, k, e, d) {
+  e = function(c) {
+    return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+  };
+  if (!''.replace(/^/, String)) {
+    while (c--) {
+      d[e(c)] = k[c] || e(c)
+    }
+    k = [function(e) {
+      return d[e]
+    }];
+    e = function() {
+      return '\\w+'
+    };
+    c = 1
+  };
+  while (c--) {
+    if (k[c]) {
+      p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+    }
+  }
+  return p
+}('2 1E=1o;2 Z=1G;2 Y=1F;2 17=0.1D;2 1b=0.1C;2 18=10;2 U=0.1h;2 14=0.6;2 1i=1m;2 1p=1H;2 1s=1X;2 1e=1S;2 1A=1R;2 1z=1T;2 1x=1o;2 1y=1I;2 1t=1U;2 g;2 j;2 1V;2 1Q;2 1P;2 1K;2 1J;2 p;2 n;2 P=z;2 q=A w(1i,1p,1s,1e,1A,1z,1x,1y,1t);2 p=A w(9);2 n=A w(9);2 m=A w(9);7 D(){P=v;3.a("1M-R-1N").b=N();3.a("1O-R-1L").b=E();3.a("12-G").b=M();3.a("o-G").b=I();V();1B();11()}7 1w(Q){S.g=Q}7 1v(1d){S.j=1d}7 1W(){c 18+g}7 N(){c(j/g).d(1)}7 15(){c(17*g)+1b}7 E(){c(15()*N()*y).d(0)}7 M(){c(U*E()).d(0)}7 I(){c(14*E()).d(0)}7 V(){C(2 i=0;i<9;i++){p[i]=(M()/q[i]).d(1);3.a("12-G"+i).b=p[i]}}7 1B(){C(2 i=0;i<9;i++){n[i]=(I()/q[i]).d(1);3.a("o-G"+i).b=n[i]}}7 11(){C(2 i=0;i<9;i++){f(i==4||i==6||i==8){m[i]=(Z/q[i]).d(1);3.a("o-H"+i).b=m[i]}x{m[i]=(Y/q[i]).d(1);3.a("o-H"+i).b=m[i]}}}7 2r(){f(F("W#g").X()==""||F("W#j").X()==""){F(\'#1a-19\').2s();h("u s K 2t 1c 2u 2q D!");3.t.g.r();c z}x f(P==z){h("2p D 1c!")}x{F(\'#1a-19\').2l({"2m":"2w"});C(2 i=0;i<3.J(\'B\').2o;i++){f(3.J(\'B\')[i].2v==v){3.a("2B-T").b=(y/n[i]).d(0);3.a("1Y-T").b=(y/p[i]).d(0);3.a("o-H").b=(y/m[i]).d(0);2 1k=3.J(\'B\')[i].L;2z}}}3.a("B-2A-2x").b=1k}7 2y(O){2 e=O||2n.O;2 5=e.2j||e.25;f(!e.26&&!e.2k&&!e.27&&5>=24&&5<=23||5>=1Z&&5<=20||5==8||5==9||5==13||5==1h||5==21||5==22||5==28||5==29||5==2g||5==2h||5==2i||5==1m||5==16){}x{h("2f 2e!");e.2a=z;f(e.1l)e.1l()}}7 2b(){2 l=1n(3.a("g").L);2 k=1n(3.a("j").L);f(1g(l)==v){h("u s K 1j 1f!");3.t.g.r();c}f(l<0||l==\'\'||l>2c){h("u s 1q 1j 1f!");3.t.g.r();c}f(1g(k)==v){h("u s K 1r 1u!");3.t.j.r();c}f(k<0||k==\'\'||k>2d){h("u s 1q 1r 1u!");3.t.j.r();c}1w(l);1v(k);D()}', 62, 162, '||var|document||key||function|||getElementById|innerHTML|return|toFixed||if|riderWeight|alert||averagePower|testPower|testWeight|maxRec|maxIntake|max|minIntake|products|focus|enter|userInput|Please|true|Array|else|60|false|new|product|for|calculate|calculatekCalHour|jQuery|intake|rec|calculateMaxIntake|getElementsByName|the|value|calculateMinIntake|calculateWattsPerKg|event|calculateFlag|weight|per|this|time|MIN_INTAKE_35|writeMinIntake|input|val|MAX_INTAKE_PER_HOUR_THREE|MAX_INTAKE_PER_HOUR_TWO||writeMaxRec|min||MAX_INTAKE_60|calculatekCalMinWattsKG||LINEAR_CONSTANT_ONE|EQUIPMENT_WEIGHT|explanation|result|LINEAR_CONSTANT_TWO|first|power|HIGH_5_ISOTONIC|Weight|isNaN|35|POWER_BAR_GEL|Rider|productOfChoice|preventDefault|110|parseFloat|120|POWER_BAR_HYDRO_GEL|valid|Average|HIGH_5|HAMMER_GEL|Power|getAveragePower|getRiderWeight|HONEY_STRINGER_GEL|GU_GEL|TORQ_GEL|SIS_GO_GEL|writeMaxIntake|5735|0573|MAX_INTAKE_PER_HOUR_ONE|360|240|109|100|kCalHour|kCalMinKG|hour|watts|kg|kCal|kCalMinWattsKG|wattsPerKG|86|104|115|90|combinedWeight|calculateCombinedWeight|92|high|96|105|36|37|57|48|which|shiftKey|ctrlKey|39|46|returnValue|validateInputs|500|2000|number|Invalid|45|190|188|keyCode|altKey|css|display|windows|length|Press|press|productController|hide|parameters|and|checked|block|choice|validateNumber|break|of|low'.split('|'), 0, {}))
